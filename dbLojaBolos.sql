1207b0a8572cfc8516e853e0f131cab1b45359e3
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-04-17 20:57:50 -0300

    Add files via upload

---
+++ dbLojaBolos.sql
@@ -0,0 +1,19 @@
+-- drop database dbLojaBolos;
+
+create database dbLojaBolos;
+
+use dbLojaBolos;
+
+create table tbProdutos(
+codProd int not null auto_increment,
+nome varchar(100) not null,
+preco decimal(9,2) not null,
+descricao varchar(100),
+primary key (codProd)
+); 
+
+desc tbProdutos;
+
+insert into tbProdutos(nome,preco,descricao) values();
+
+select * from tbProdutos;
