0c12de8e8cfa74a782e729620ad327df1e4fe3b3
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-04-22 22:07:31 -0300

    Add files via upload

--- dbLojaBolos.sql
+++ dbLojaBolos.sql
@@ -12,16 +12,26 @@ descricao varchar(100),
 primary key (codProd)
 ); 
 
+create table tbVendas(
+codVend int not null auto_increment,
+dataVenda datetime,
+valor varchar(100),
+codProd int not null,
+primary key (codVend),
+foreign key (codProd) references tbProdutos(codProd)
+);
+
 desc tbProdutos;
+desc tbVendas;
 
---insert into tbProdutos(nome,preco,descricao) values();
+-- insert into tbProdutos(nome,preco,descricao) values();
 
-select * from tbProdutos;
+-- select codProd as Codigo, nome as Produto, preco as Preco, descricao as Descrição from tbProdutos;
 
---select nome from tbProdutos;
+-- select nome from tbProdutos;
 
---select * from tbProdutos where preco = preco;
+-- select * from tbProdutos where preco = preco;
 
---update tbProdutos set nome = nome,preco = preco,descricao = descricao where codProd = codProd;
+-- update tbProdutos set nome = nome,preco = preco,descricao = descricao where codProd = codProd;
 
-delete from tbProdutos where codProd = codProd;
+-- delete from tbProdutos where codProd = codProd;
