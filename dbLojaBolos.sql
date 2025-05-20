acb1649bbe53e90e7ad67b6e5545dd08984c2d4c
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-05-19 21:41:36 -0300

    Finalizado

--- dbLojaBolos.sql
+++ dbLojaBolos.sql
@@ -4,6 +4,18 @@ create database dbLojaBolos;
 
 use dbLojaBolos;
 
+create table tbRegistro(
+codReg int not null auto_increment,
+data date,
+codigo int,
+preco decimal(9,2),
+nome varchar(100),
+quantidade char(100),
+valorTotal decimal(9,2),
+totalVenda varchar(100),
+primary key (codReg)
+);
+
 create table tbProdutos(
 codProd int not null auto_increment,
 nome varchar(100) not null,
@@ -22,27 +34,9 @@ codProd INT,
 CONSTRAINT fk_codProd FOREIGN KEY (codProd) REFERENCES tbProdutos(codProd) ON DELETE SET NULL
 );
 
+
 desc tbProdutos;
 desc tbVendas;
+desc tbRegistro;
 
--- insert into tbProdutos(nome,preco,descricao) values('matheus',100,'abc');
-
--- insert into tbVendas(dataVenda,valor,quantidade,codProd) values('2025/04/25',150,'12',1);
-
--- insert into tbVendas(valor) values();
-
--- insert into tbProdutos(nome,preco,descricao) values();
-
--- select codProd as Codigo, nome as Produto, preco as Preco, descricao as Descrição from tbProdutos;
-
--- select nome from tbProdutos;
-
--- select * from tbProdutos where preco = preco;
-
--- update tbProdutos set nome = nome,preco = preco,descricao = descricao where codProd = codProd;
-
--- delete from tbProdutos where codProd = codProd;
-
--- select p.nome, v.valor,v.quantidade from tbVendas as v inner join tbProdutos as p on v.codProd = p.codProd;
-
-select * from tbVendas;
+select * from tbRegistro;
